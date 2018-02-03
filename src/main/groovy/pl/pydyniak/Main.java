ade8d05509dbd63fa077dc93825fcb80b693b422
Rafał Pydyniak <xardi55@gmail.com>
2018-02-03 22:11:42 +0100

    Path to images passed in argument + simple check of this path

--- src/main/groovy/pl/pydyniak/Main.java
+++ src/main/groovy/pl/pydyniak/Main.java
@@ -1,11 +1,22 @@
 package pl.pydyniak;
 
+import java.io.File;
+
 /**
  * Created by rafal on 5/26/16.
  */
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Podaj sciezke w parametrze");
+        }
+        String path = args[0];
+        System.out.println(path);
+        File directoryFile = new File(path);
+        if (!directoryFile.exists() || !directoryFile.canRead()) {
+            throw new IllegalArgumentException("Katalog nie istnieje lub brak uprawnien do odczytu");
+        }
         ImagesRename imagesRename = new ImagesRename();
-        imagesRename.renameImagesToCreationDate("/media/rafal/01D1A041EEE839D0/Zdjecia/2016.05 Wlochy/Wenecja");
+        imagesRename.renameImagesToCreationDate(path);
     }
 }
