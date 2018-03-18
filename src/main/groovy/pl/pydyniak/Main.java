2ae14b655eed1ed9cc34d1a46a20b2a2e904343f
Rafał Pydyniak <xardi55@gmail.com>
2018-03-18 19:44:14 +0100

    Instead of passing path in parameter user is asked for it

--- src/main/groovy/pl/pydyniak/Main.java
+++ src/main/groovy/pl/pydyniak/Main.java
@@ -1,20 +1,21 @@
 package pl.pydyniak;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by rafal on 5/26/16.
  */
 public class Main {
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Podaj sciezke w parametrze");
-        }
-        String path = args[0];
-        System.out.println(path);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter path: ");
+        String path = br.readLine();
         File directoryFile = new File(path);
         if (!directoryFile.exists() || !directoryFile.canRead()) {
-            throw new IllegalArgumentException("Katalog nie istnieje lub brak uprawnien do odczytu");
+            throw new IllegalArgumentException("The catalog doesn't exist or can't be read due to lack of permissions");
         }
         ImagesRename imagesRename = new ImagesRename();
         imagesRename.renameImagesToCreationDate(path);
